4d2f68ee6b230738d3050a40e8ee1e4b0c5a2760
hanamaru03 <marik0saku315+github@gmail.com>
2026-07-02 21:05:21 +0900

    Dateクラスのインスタンスを使用する方法を取り入れた

--- 02.calendar/cal.rb
+++ 02.calendar/cal.rb
@@ -13,25 +13,21 @@ opt.on('-m MONTH'){|v| month = v.to_i}
 
 opt.parse!(ARGV)
 
-check_last_date = Date.new(year, month, -1)
-last_day = check_last_date.day
-wday = Date.new(year, month, 1).wday
+first_day = Date.new(year, month, 1)
+last_day = Date.new(year, month, -1)
 
 week1 = []
-if wday != 0
-  week1.push(("  ") + ("   " * (wday - 1)))
-end
-
-(1..last_day).each {|num|
-  if (num + wday) % 7 == 0
-    week1.push(num.to_s.rjust(3) + "\n")
-  elsif Date.new(year, month, num).wday == 0
-    week1.push(num.to_s.rjust(2))
-  else
-    week1.push(num.to_s.rjust(3))
-  end
-}
 
 puts "#{year}年 #{month}月".center(18)
 puts "日 月 火 水 木 金 土"
-puts week1.join
+print (['   '] * first_day.wday).join
+
+(first_day..last_day).each {|date|
+  week1.push(date.day.to_s.rjust(2))
+  if date.saturday?
+    puts week1.join(' ')
+    week1 = []
+  end
+}
+
+puts week1.join(' ')
